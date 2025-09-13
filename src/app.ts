26eb990ef67471d47cf247cd01e94764c654bb2f
khanh071006 <donggiakhanh123456@gmail.com>
2025-09-13 17:07:48 +0700

    video 48

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express"
+
 require('dotenv').config()
 
 const app = express();
+//config view engine
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
 
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-    res.send("KhHeheheh sd")
+    res.render("home.ejs")
 })
 
 app.listen(8080, () => {
